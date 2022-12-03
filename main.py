bb9bd15650fdb4f47b2a3780c6812f370e5d7101
Michael Livingston <104595128+livingstoniv@users.noreply.github.com>
2022-12-03 11:28:55 -0900

    Update main.py
    Edited code to show percentage of total allocation of long vs short volume and open interest, rounded that to two decimal places, and then edited the exporting of the Excel sheet to allow column titles to fit in the Excel sheet cells.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from yahoo_fin import options as op
 import pandas as pd
 from datetime import datetime, date
 import numpy as np
+from yahoo_fin import stock_info as si
+from xlsxwriter.utility import xl_rowcol_to_cell
+import openpyxl as px
 
 #Empty Lists For Data
 expirationDate = []
@@ -11,6 +14,11 @@ totalLongVolumeByExpiration = []
 totalShortVolumeByExpiration = []
 totalLongOpenInterestByExpiration = []
 totalShortOpenInterestByExpiration = []
+longOpenIntAsPerc = []
+shortOpenIntAsPerc = []
+longVolAsPerc = []
+shortVolAsPerc = []
+
 
 #Input For Finding Stock Info, Run The Program Then Enter The Ticker Into The Terminal
 ticker = input('Enter your ticker here (make sure it has options data): ')
@@ -46,6 +54,20 @@ for i in range(0, len(expDates)):
     totalShortVol = chain['puts']['Volume'].sum()
     totalShortInt = chain['puts']['Open Interest'].sum()
     
+    totalVol = totalLongVol + totalShortVol 
+    totalOpenInt = totalLongInt + totalShortInt
+
+    longVolPercent = 100 * totalLongVol / totalVol
+    shortVolPercent = 100 * totalShortVol / totalVol
+
+    longOpenIntPecent = 100 * totalLongInt / totalOpenInt
+    shortOpenIntPercent = 100 * totalShortInt / totalOpenInt
+
+    roundLongvolPerc = round(longVolPercent, 2)
+    roundShortvolPerc = round(shortVolPercent, 2)
+    roundLongOpenInt = round(longOpenIntPecent, 2)
+    roundShortOpenInt = round(shortOpenIntPercent, 2)
+    
 #Appends Expiration Date Info, Open Interest Info, and Volume Info
     expirationDate.append(expDate.strftime('%m/%d/%Y'))
     daysToExpiration.append(DTE)
@@ -53,10 +75,25 @@ for i in range(0, len(expDates)):
     totalShortVolumeByExpiration.append(totalShortVol)
     totalLongOpenInterestByExpiration.append(totalLongInt)
     totalShortOpenInterestByExpiration.append(totalShortInt)
+    longVolAsPerc.append(roundLongvolPerc)
+    shortVolAsPerc.append(roundShortvolPerc)
+    longOpenIntAsPerc.append(roundLongOpenInt)
+    shortOpenIntAsPerc.append(roundShortOpenInt)
 #Interates The On-Going Expiration Dates Until Complete
     print('Finished with ' + expDate.strftime('%m/%d/%Y') + ' expiration.')
 
 byExpirationData = pd.DataFrame(data = list(zip(expirationDate, daysToExpiration, totalLongVolumeByExpiration, totalLongOpenInterestByExpiration, totalShortVolumeByExpiration, totalShortOpenInterestByExpiration)), columns= ['Expiration Date', 'DTE', 'Total Long Volume', 'Total Long Open Interest', 'Total Short Volume', 'Total Short Open Interest'])
 print(byExpirationData)
-#Exports Dataframe To An Excel Sheet, Feel Free to Change The Name Based On Your Ticker
-byExpirationData.to_excel('AssetData.xlsx', sheet_name='Asset Data')
+#Exports Dataframe To An Excel Sheet, Inputs Are Used To Make Naming The File In The Terminal Easier
+file_name = input("Enter Your File Name Here: ")
+sheet_name_var = input("Enter The Name Of Your Sheet Here: ")
+file_extension = '.xlsx'
+
+#Code to make column titles in excel sheet fit in Excel sheet cells
+writer = pd.ExcelWriter(file_name + file_extension, engine='xlsxwriter')
+byExpirationData.to_excel(writer, sheet_name=sheet_name_var, index=False, na_rep='NaN')
+for column in byExpirationData:
+    column_length = max(byExpirationData[column].astype(str).map(len).max(), len(column))
+    col_idx = byExpirationData.columns.get_loc(column)
+    writer.sheets[sheet_name_var].set_column(col_idx, col_idx, column_length)
+writer.save()
